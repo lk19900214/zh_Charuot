25fffce159f5b20048bdc550284e95b40a4e2f57
lk19900214 <1493617317@qq.com>
2017-09-28 19:18:53 +0800

    登录

---
+++ src/main/java/com/zh/mapper/UserDoMapper.java
@@ -0,0 +1,9 @@
+package com.zh.mapper;
+
+import com.zh.entity.UserDo;
+import com.zh.util.MyMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserDoMapper extends MyMapper<UserDo> {
+}
